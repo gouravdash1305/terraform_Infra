93f9c51549f5bf93f223b8d99ebadf2da8ce130f
Gourav Dash <gouravdash.1305@gmail.com>
2022-08-04 11:14:39 +0530

    removed the aws_key_pair block

--- instance.tf
+++ instance.tf
@@ -1,9 +1,9 @@
 # Creating key pair which will be attached the instance.
 
-resource "aws_key_pair" "my-key" {
-  key_name   = "kali-key"
-  # public_key = file("${path.module}/id_rsa.pub")
-}
+# resource "aws_key_pair" "my-key" {
+#   key_name   = "kali-key"
+#   # public_key = file("${path.module}/id_rsa.pub")
+# }
 
 # output "keyname" {
 #   value = aws_key_pair.my-key.key_name
